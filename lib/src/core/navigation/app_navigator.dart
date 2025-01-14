ec5644442ee3f84136c8b235cb1299f61fef0876
JHProyectos <129443750+JHProyectos@users.noreply.github.com>
2025-01-14 12:53:57 -0300

    Update app_navigator.dart

--- lib/src/core/navigation/app_navigator.dart
+++ lib/src/core/navigation/app_navigator.dart
@@ -1,1 +1,40 @@
+//lib/src/core/navigation/app_navigator.dart
+import 'package:flutter/material.dart';
+import 'app_routes.dart';
+import 'page_transition.dart';
 
+class AppNavigator {
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  static Future<T?> pushNamed<T>(AppRoute route, {Object? arguments}) {
+    return navigatorKey.currentState!.pushNamed(route.path, arguments: arguments);
+  }
+
+  static Future<T?> pushReplacementNamed<T>(AppRoute route, {Object? arguments}) {
+    return navigatorKey.currentState!.pushReplacementNamed(route.path, arguments: arguments);
+  }
+
+  static void pop<T>([T? result]) {
+    return navigatorKey.currentState!.pop(result);
+  }
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    final route = AppRoute.values.firstWhere(
+      (r) => r.path == settings.name,
+      orElse: () => AppRoute.home,
+    );
+
+    switch (route) {
+      case AppRoute.login:
+      case AppRoute.home:
+        return FadePageRoute(builder: (_) => route.page, settings: settings);
+      case AppRoute.utnValidation:
+      case AppRoute.pdfUpload:
+      case AppRoute.settings:
+        return SlidePageRoute(builder: (_) => route.page, settings: settings);
+      case AppRoute.processing:
+      case AppRoute.playback:
+        return FadePageRoute(builder: (_) => route.page(settings.arguments), settings: settings);
+    }
+  }
+}
