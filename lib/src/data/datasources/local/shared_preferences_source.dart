da47ef2ec7626ee856432851b92b4411034c70c8
JHProyectos <129443750+JHProyectos@users.noreply.github.com>
2025-01-15 10:32:03 -0300

    Update shared_preferences_source.dart

--- lib/src/data/datasources/local/shared_preferences_source.dart
+++ lib/src/data/datasources/local/shared_preferences_source.dart
@@ -1,1 +1,34 @@
 //lib/src/data/datasources/remote/firebase_source.dart
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesSource {
+  late SharedPreferences _prefs;
+
+  Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<bool> setString(String key, String value) async {
+    return await _prefs.setString(key, value);
+  }
+
+  String? getString(String key) {
+    return _prefs.getString(key);
+  }
+
+  Future<bool> setBool(String key, bool value) async {
+    return await _prefs.setBool(key, value);
+  }
+
+  bool? getBool(String key) {
+    return _prefs.getBool(key);
+  }
+
+  Future<bool> remove(String key) async {
+    return await _prefs.remove(key);
+  }
+
+  Future<bool> clear() async {
+    return await _prefs.clear();
+  }
+}
