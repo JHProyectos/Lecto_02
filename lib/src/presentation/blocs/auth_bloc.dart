ac4c46a1325a4cc84c620d84f35e0b0448464370
JHProyectos <129443750+JHProyectos@users.noreply.github.com>
2025-01-16 11:07:16 -0300

    Update auth_bloc.dart

--- lib/src/presentation/blocs/auth_bloc.dart
+++ lib/src/presentation/blocs/auth_bloc.dart
@@ -1,1 +1,63 @@
-####
+//lib/src/presentation/blocs/auth_bloc.dart
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../domain/usecases/login_usecase.dart';
+import '../../domain/entities/user.dart';
+
+/// Eventos del AuthBloc
+abstract class AuthEvent {}
+
+class LoginEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  LoginEvent({required this.email, required this.password});
+}
+
+class LogoutEvent extends AuthEvent {}
+
+/// Estados del AuthBloc
+abstract class AuthState {}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthAuthenticated extends AuthState {
+  final User user;
+
+  AuthAuthenticated(this.user);
+}
+
+class AuthUnauthenticated extends AuthState {}
+
+class AuthError extends AuthState {
+  final String message;
+
+  AuthError(this.message);
+}
+
+/// BLoC para manejar la autenticación
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final LoginUseCase loginUseCase;
+
+  AuthBloc({required this.loginUseCase}) : super(AuthInitial()) {
+    on<LoginEvent>(_onLogin);
+    on<LogoutEvent>(_onLogout);
+  }
+
+  /// Maneja el evento de inicio de sesión
+  Future<void> _onLogin(LoginEvent event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final user = await loginUseCase.execute(event.email, event.password);
+      emit(AuthAuthenticated(user));
+    } catch (e) {
+      emit(AuthError('Error de autenticación: ${e.toString()}'));
+    }
+  }
+
+  /// Maneja el evento de cierre de sesión
+  void _onLogout(LogoutEvent event, Emitter<AuthState> emit) {
+    emit(AuthUnauthenticated());
+  }
+}
