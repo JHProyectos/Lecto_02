06b505f0e15e3869d2781554dea0d7f3994837f2
JHProyectos <129443750+JHProyectos@users.noreply.github.com>
2025-01-16 11:04:27 -0300

    Update custom_button.dart

--- lib/src/presentation/widgets/custom_button.dart
+++ lib/src/presentation/widgets/custom_button.dart
@@ -1,1 +1,50 @@
+//lib/src/presentation/widgets/custom_button.dart
+import 'package:flutter/material.dart';
 
+/// Un botón personalizado que se puede utilizar en toda la aplicación.
+class CustomButton extends StatelessWidget {
+  /// El texto que se mostrará en el botón.
+  final String text;
+
+  /// La función que se ejecutará cuando se presione el botón.
+  final VoidCallback onPressed;
+
+  /// El color de fondo del botón.
+  final Color? backgroundColor;
+
+  /// El color del texto del botón.
+  final Color? textColor;
+
+  /// Indica si el botón está habilitado o no.
+  final bool isEnabled;
+
+  /// Constructor del CustomButton.
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.backgroundColor,
+    this.textColor,
+    this.isEnabled = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: isEnabled ? onPressed : null,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor ?? Theme.of(context).primaryColor,
+        foregroundColor: textColor ?? Colors.white,
+        disabledBackgroundColor: Colors.grey,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
